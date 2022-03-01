2605b37e37feb0df3a9ec16f5664f977432934aa
davay <fond@fondfrukt.ru>
2022-03-02 00:12:18 +0300

    dep

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd _dist
 
 git init
-git remote add origin git@github.com:ooley42/ooley.ru.git 
+git remote add origin git@github.com:ooley42/ooley.git 
 git add . --force
 git commit -m 'deploy'
 

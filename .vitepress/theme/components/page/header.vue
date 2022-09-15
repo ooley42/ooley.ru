d83542f8940f5aa003fb70b203546b2b67465ae9
davay <fond@fondfrukt.ru>
2022-09-15 23:35:53 +0300

    bump

--- .vitepress/theme/components/page/header.vue
+++ .vitepress/theme/components/page/header.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useData, useRoute } from 'vitepress'
+import { useData } from 'vitepress'
 
 const { theme } = useData();
 
@@ -9,7 +9,7 @@ const { theme } = useData();
 header#start
   .flex.flex-col.items-center
     a.no-underline.p-4.flex-auto.flex.items-center.justify-center(href="/" title="Go home")
-      img.w-14rem(:src="theme.logo" alt="OOLEY logo")
+      img.w-14rem(:src="theme.logo" alt="OOLEY")
     .flex.flex-col.w-full.justify-center.max-w-3xl
 
       nav-bar
@@ -20,9 +20,10 @@ header#start
 <style lang="postcss" scoped>
 header {
   flex: 1 1 100%;
-  background-color: v-bind(theme.color);
+  background-color: #f9a02c;
   @apply flex flex-col z-2 shadow-lg;
 }
+
 .info {
   @apply flex flex-col items-center gap-2 w-full p-4 bg-light-600 dark_bg-dark-500;
 }
